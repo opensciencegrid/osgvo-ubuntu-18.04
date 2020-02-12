e532b4f829bb2eba9f6d40475efa03c3f23bc0c4
Mats Rynge <rynge@isi.edu>
2020-02-12 08:34:51 -0800

    PS1 fix

--- .singularity.d/env/90-environment.sh
+++ .singularity.d/env/90-environment.sh
@@ -1,8 +1,6 @@
 # Custom environment shell code should follow
 
-if [ "x$LD_LIBRARY_PATH" = "x" ]; then
-    export LD_LIBRARY_PATH="/host-libs"
-else
-    export LD_LIBRARY_PATH="/host-libs:$LD_LIBRARY_PATH"
-fi
+# ensure we have PS1 set
+PS1="Singularity $SINGULARITY_NAME:\\w> "
+export PS1
 
